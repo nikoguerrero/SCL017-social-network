39d036ad350374b00809a44e101ff1dce6431fd9
nikoguerrero <nclguerrero@gmail.com>
2021-07-12 21:07:16 -0400

    eslint corrections in firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,70 +1,67 @@
-
-//función de inicializar firebase
-function firebaseInit(){
-  var firebaseConfig = {
-      apiKey: "AIzaSyC7VRqwv_KqG8k7lA6EpUuPIvQ70r-jafY",
-      authDomain: "bearhug-ca9c3.firebaseapp.com",
-      projectId: "bearhug-ca9c3",
-      storageBucket: "bearhug-ca9c3.appspot.com",
-      messagingSenderId: "518796083283",
-      appId: "1:518796083283:web:2a47b273fcd1b933e586a6",
-      measurementId: "G-XEMSLEFBF3"
-    };
-    
-    firebase.initializeApp(firebaseConfig);
+// función de inicializar firebase
+function firebaseInit() {
+  const firebaseConfig = {
+    apiKey: 'AIzaSyC7VRqwv_KqG8k7lA6EpUuPIvQ70r-jafY',
+    authDomain: 'bearhug-ca9c3.firebaseapp.com',
+    projectId: 'bearhug-ca9c3',
+    storageBucket: 'bearhug-ca9c3.appspot.com',
+    messagingSenderId: '518796083283',
+    appId: '1:518796083283:web:2a47b273fcd1b933e586a6',
+    measurementId: 'G-XEMSLEFBF3'
+  };
+  firebase.initializeApp(firebaseConfig);
 }
 
-//función de hacer login con firebase
+// función de hacer login con firebase
 function firebaseLogin(email, password) {
   firebase.auth().signInWithEmailAndPassword(email, password)
     .then((userCredential) => {
-      // Signed in
-      var user = userCredential.user;
-      // ...
-      })
-      .catch((error) => {
-
-      var errorCode = error.code;
-      var errorMessage = error.message;
-
-      window.alert("Error : " + errorMessage);
-      });
-}
-
-//función de hacer login a través de Google con Firebase
-function firebaseGoogleLogin(){
-  const provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth()
-      .signInWithPopup(provider)
-      .then((result) => {
-          console.log('google signed in')
-      })
-      .catch(err => {
-          console.log(err)
-      });
-}
-
-//función de salir del login con firebase
-function firebaseLogout() {
-  firebase.auth().signOut().then(() => {
-      window.alert('deslogueado');
-    }).catch((error) => {
-      console.error(error);
-      // An error happened.
+    // Signed in
+      const user = userCredential.user;
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      window.alert('Error : ' + errorMessage);
     });
 }
 
-//función de registrar al usuario con firebase
+// función de hacer login a través de Google con Firebase
+function firebaseGoogleLogin() {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth()
+    .signInWithPopup(provider)
+    .then((result) => {
+      console.log('google signed in');
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+// función de salir del login con firebase
+function firebaseLogout() {
+  firebase.auth().signOut()
+  .then(() => {
+    window.alert('deslogueado');
+  })
+  .catch((error) => {
+    console.error(error);
+      // An error happened.
+  });
+}
+
+// función de registrar al usuario con firebase
 function firebaseRegisterUser(email, password) {
   firebase.auth().createUserWithEmailAndPassword(email, password)
     .then((userCredential) => {
       // Signed in
-      var user = userCredential.user;
+      const user = userCredential.user;
       // ...
     })
     .catch((error) => {
-      var errorCode = error.code;
-      var errorMessage = error.message;
+      const errorCode = error.code;
+      const errorMessage = error.message;
       // ..
     });
 }
