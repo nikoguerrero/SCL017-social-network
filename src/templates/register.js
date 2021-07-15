035abcda115881b75f81c3d789490bbd6f705e1c
GabbyHolics <karenthgabriela@gmail.com>
2021-07-15 17:57:29 -0400

    register

--- src/templates/register.js
+++ src/templates/register.js
@@ -4,12 +4,15 @@ export const registerTemplate = () => {
   const containerRegister = document.createElement("div");
   containerRegister.className = "containerRegisterClass"
   const signUp = `
-    <h3>BearHug</h3>
+  <div class="homeImage">
+  <img src="images/logotipo.png" class="logotipo">
+  </div>
     <input type="text" id= "name_field" class="emailBox" placeholder="Ingresa tu nombre">
     <input type="email" id="signUpEmail" class="emailBox" placeholder="Ingresa tu correo">
     <input type="password" id="signUpPass" class="passwordBox" placeholder="Ingresa tu contraseña">
     <button class="buttonLog"> <a href="#/muro" id="registerButton">Registrar</a> </button>
-    </div>`;
+    <p class="condition">Al hacer clic en "Registrarte", aceptas nuestras Condiciones, la Política de datos 
+    y la Política de cookies. </p>`;
 containerRegister.innerHTML = signUp;
 const registerButton = containerRegister.querySelector('#registerButton');
 registerButton.addEventListener('click', (e) => {
