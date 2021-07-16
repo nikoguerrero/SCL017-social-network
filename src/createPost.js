090708d06cac759ec295a245ac353335b4503c9d
GabbyHolics <karenthgabriela@gmail.com>
2021-07-16 09:26:40 -0400

    add create post template

---
+++ src/createPost.js
@@ -0,0 +1,12 @@
+export const postTemplate = () => {
+    const containerAddPost = document.createElement("section");
+    containerAddPost.className = "containerAddPost";
+
+const addPost = `<a href="#feed" id="goBack" class="backLink"></a>
+<input type="text" placeholder="¿Como te sientes hoy?" id="postInput" class="postBox">
+<button id="postButton" class="postButtonLink"></button>`;
+
+containerAddPost.innerHTML= addPost;
+return containerAddPost;
+
+};
