0943e977c591ffe4cbdb7559e5e7d234213b64ff
GabbyHolics <karenthgabriela@gmail.com>
2021-07-20 14:23:52 -0400

    array post

--- src/createPost.js
+++ src/createPost.js
@@ -1,20 +1,38 @@
 export const postTemplate = () => {
   const containerAddPost = document.createElement('section');
+  const publicPost = document.createElement('div')
+  publicPost.id = ("#publicPost")
+  const publicPostView = document.querySelector('#publicPost')
+ 
   containerAddPost.className = 'containerAddPost';
 
   const addPost = `
   <div class="containerPost">
   <a href="#feed" id="goBack" class="backLink"> Volver al feed</a>
   <div class="formPost">
-  <input type="text" placeholder="¿Cómo te sientes hoy?" id="postInput" class="postBox">
-  </div>
   <textarea id="text-description" class="form-control" placeholder="Descríbelo aquí"></textarea>
+  </div>
   <button id="postButton" class="postButtonLink"> enviar </button>`;
 
   containerAddPost.innerHTML = addPost;
+  containerAddPost.appendChild(publicPost)
+  const viewPost = (doc) =>{
+    let li = document.createElement('li');
+    let text = document.createElement('span');
+
+    li.setAttribute('data-id', doc.id);
+    text.textContent = doc.data().tetextDescription;
+     li.appendChild(text)
+  publicPost.appendChild(containerAddPost)
+
+  };
    const db = firebase.firestore()
+
+
    const getPost = db.collection('post').get().then((snapshot) =>{
-      console.log(snapshot.docs);
+     snapshot.docs.forEach(doc => {
+            console.log(doc.data())
+     });
    });
   //  window.addEventListener('DOMContentLoaded', async (e) =>{
   //   await getPost();
@@ -26,14 +44,12 @@ export const postTemplate = () => {
   const postButton = containerAddPost.querySelector('#postButton');
   postButton.addEventListener('click', async (e) => {
   e.preventDefault()
-  const postInput = document.querySelector('#postInput').value;
   const textDescription = document.querySelector('#text-description').value;
   const response = await db.collection('post').doc().set({
-    postInput,
     textDescription
   });   
   console.log(response)                                                                                         
-  console.log(postInput,textDescription)
+  console.log(textDescription)
   });
   return containerAddPost;
 };
