b440cc06cdaefc442a6b9dafe60c865588fcec7e
GabbyHolics <karenthgabriela@gmail.com>
2021-07-23 13:25:21 -0400

    1

--- src/createPost.js
+++ src/createPost.js
@@ -15,24 +15,26 @@ export const postTemplate = () => {
   containerAddPost.innerHTML = addPost;
 
   const db = firebase.firestore(); 
-
- 
+  let editPostId = null // declara que el id post a editar es seteado  a nulo, para que no reescriba ningun post 
 
   const viewPost = (doc) => {
     let li = document.createElement('li');
     li.className = ('li');
-    let textDescription = document.createElement('span');
-    textDescription.className = ('textDescription')
+    let postedText = document.createElement('span');
+    postedText.className = ('postedText');
+    postedText.id = ('postedTextId');
     let cross = document.createElement('div');
     cross.className = ('delete');
-    let edit = document.createElement('div');
-    edit.className = ('edit');
-    edit.textContent = 'editar';
-    li.setAttribute('data-id', doc.id);
-    textDescription.textContent = doc.data().textDescription;
-    cross.textContent = 'X';
+    let edit = document.createElement('button');
+    edit.className = ('edit')
 
-    li.appendChild(textDescription);
+    li.setAttribute('data-id', doc.id);
+    postedText.textContent = doc.data().textDescription;
+    cross.textContent = 'X';
+    edit.textContent = 'editar';
+
+
+    li.appendChild(postedText);
     li.appendChild(cross);
     li.appendChild(edit);
     publicPost.appendChild(li);
@@ -44,6 +46,14 @@ export const postTemplate = () => {
       let textId = e.target.parentElement.getAttribute('data-id');
       db.collection('post').doc(textId).delete();
     });
+    
+    edit.addEventListener('click',async (e) => {
+      e.stopPropagation();
+      editPostId = e.target.parentElement.getAttribute('data-id'); // guardamos el id del post
+      const postData =  await  db.collection('post').doc(editPostId).get(); // pasamos la data del post a la variable postData
+      textDescription.value = postData.data().textDescription;
+    });
+
   };
 
   const containerPost = containerAddPost.querySelector('#containerPost');
@@ -54,26 +64,39 @@ export const postTemplate = () => {
     if (textDescription.value.length == '') {
       alert('Recuerda, para conectar necesitas experesarte ');
     } else {
-      const response = await db.collection('post').add({
-        textDescription: textDescription.value
-      });
+      if( editPostId === null){ // si no hay post a editar, agrega un nuevo post
+        await db.collection('post').add({
+          textDescription: textDescription.value
+        });
+    } else { // cuando se edita, se modifica el post selecionado 
+       await db.collection('post').doc(editPostId).set({
+          textDescription: textDescription.value
+        });
+      editPostId = null;
+    }
   }
     textDescription.value = '';
-    console.log(response);
+    
   });
 
   // real-time listener
   db.collection('post').onSnapshot(snapshot => {
     let changes = snapshot.docChanges();
     changes.forEach(change => {
-      console.log(change.doc.data());
-      if (change.type == 'added') {
+      // console.log(change.doc.data());
+      if (change.type === 'added') {
         viewPost(change.doc);
-      } else if (change.type == 'removed') {
+      } else if (change.type === 'modified'){
+        let li = publicPost.querySelector('[data-id=' + change.doc.id + ']');
+        li.querySelector('#postedTextId').textContent = change.doc.data().textDescription; // reescribir de forma inmediata el texte area. 
+      } else if (change.type === 'removed') {
         let li = publicPost.querySelector('[data-id=' + change.doc.id + ']');
         publicPost.removeChild(li);
       }
+
     });
   });
+ 
+
   return containerAddPost;
 };
