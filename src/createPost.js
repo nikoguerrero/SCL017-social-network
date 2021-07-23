7ab4b33eb57e4e8ac4aa9b9408a768cc2edb60ca
nikoguerrero <nclguerrero@gmail.com>
2021-07-23 15:02:09 -0400

    topbar added. Creating post and navbar together

--- src/createPost.js
+++ src/createPost.js
@@ -8,30 +8,35 @@ export const postTemplate = () => {
   const addPost = `
   <div class="containerPost" id="containerPost">
   <a href="#feed" id="goBack" class="backLink"> Volver al feed</a>
-  <textarea id="text-description" class="form-control " placeholder="Descríbelo aquí"></textarea>
+  <img src="./images/ejemploperfilfoto">
+  <textarea id="text-description" class="createPostText" placeholder="Descríbelo aquí"></textarea>
   <button id="postButton" class="postButtonLink"> enviar </button>
   </div>`;
 
   containerAddPost.innerHTML = addPost;
 
   const db = firebase.firestore(); 
-
+  let editPostId = null; // se declara variable del id del post a editar inicialmente seteada a nulo para que no que se reescriba ningún post
  
 
   const viewPost = (doc) => {
     let li = document.createElement('li');
     li.className = ('li');
-    let textDescription = document.createElement('span');
-    textDescription.className = ('textDescription')
+    let postedText = document.createElement('span');
+    postedText.id = 'postTextId'; // id para poder identificarlo cuando se quiera reescribir
+    postedText.className = ('textDescription')
     let cross = document.createElement('div');
     cross.className = ('delete')
+    let edit = document.createElement('button');
 
     li.setAttribute('data-id', doc.id);
-    textDescription.textContent = doc.data().textDescription;
+    postedText.textContent = doc.data().textDescription;
     cross.textContent = 'X';
+    edit.textContent = 'editar';
 
-    li.appendChild(textDescription);
+    li.appendChild(postedText);
     li.appendChild(cross);
+    li.appendChild(edit);
 
     publicPost.appendChild(li);
     containerAddPost.appendChild(publicPost);
@@ -41,6 +46,14 @@ export const postTemplate = () => {
       e.stopPropagation();
       let textId = e.target.parentElement.getAttribute('data-id');
       db.collection('post').doc(textId).delete();
+    });
+
+    edit.addEventListener('click', async (e) => {
+      e.stopPropagation();
+      editPostId = e.target.parentElement.getAttribute('data-id'); // guarda la id del post
+      const postData = await db.collection('post').doc(editPostId).get(); // pasamos la data del post en particular a una variable
+      
+      textDescription.value = postData.data().textDescription; // leemos la data de postdata y la escribimos en la caja de textarea (textdescription)
     });
   };
 
@@ -52,21 +65,31 @@ export const postTemplate = () => {
     if (textDescription.value.length == '') {
       alert('Recuerda, para conectar necesitas experesarte ');
     } else {
-      const response = await db.collection('post').add({
-        textDescription: textDescription.value
-      });
+      if (editPostId === null) { // si no hay post a editar, entonces agrega un nuevo post (en el fondo cuando se quiere crear un nuevo post)
+        const response = await db.collection('post').add({
+          textDescription: textDescription.value
+        });
+      } else { // en caso contrario, es decir, cuando sí hay post a editar, se modifica el post seleccionado para ser editado
+        await db.collection('post').doc(editPostId).set({  //puede ser update
+            textDescription: textDescription.value
+        });
+        editPostId = null; // reseteamos editPostId a nulo porque este ya se modificó
+      }
   }
     textDescription.value = '';
-    console.log(response);
+    // console.log(response);
   });
 
   // real-time listener
   db.collection('post').onSnapshot(snapshot => {
     let changes = snapshot.docChanges();
     changes.forEach(change => {
-      console.log(change.doc.data());
+      console.log(change.type);
       if (change.type == 'added') {
         viewPost(change.doc);
+      } else if (change.type == 'modified') { // si el tipo de cambio es modificado, entonces sobreescribe el texto que contiene
+        let li = publicPost.querySelector('[data-id=' + change.doc.id + ']'); // leemos el id del post que vamos a modificar
+        li.querySelector('#postTextId').textContent = change.doc.data().textDescription; //reescribimos de forma inmediata la caja de texto únicamente
       } else if (change.type == 'removed') {
         let li = publicPost.querySelector('[data-id=' + change.doc.id + ']');
         publicPost.removeChild(li);
