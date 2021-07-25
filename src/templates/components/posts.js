460036f838c84cbb308b17c1de6f577e73efdd58
nikoguerrero <nclguerrero@gmail.com>
2021-07-25 12:25:11 -0400

    moving out functions from postTemplate

---
+++ src/templates/components/posts.js
@@ -0,0 +1,129 @@
+import { createPostTemplate } from '../composePost.js';
+
+const db = firebase.firestore();
+let editPostId = null; // declara que el id post a editar es nulo, para que no se reescriba ningún post
+
+export const postTemplate = () => {
+  const containerAddPost = document.createElement('section');
+  const publicPost = document.createElement('ul');
+  publicPost.id = ('#publicPost');
+  publicPost.className = ('containerPublicPost');
+  containerAddPost.className = 'containerAddPost';
+
+  const addPost = `
+  <div class="containerPost" id="containerPost">
+  <a href="#feed" id="goBack" class="backLink"> Volver al feed</a>
+  <img src="./images/ejemploperfilfoto.png" class="feedPicProfile"> 
+  <textarea id="text-description" class="createPostText" placeholder="Descríbelo aquí"></textarea>
+  <button id="postButton" class="postButtonLink">compartir</button>
+  </div>`;
+
+  containerAddPost.innerHTML = addPost;
+  return containerAddPost;
+};
+
+const saveDataToFirestore = () => {
+  const containerPost = containerAddPost.querySelector('#containerPost');
+  const textDescription = containerPost.querySelector('#text-description');
+  const postButton = containerAddPost.querySelector('#postButton');
+  postButton.addEventListener('click', async (e) => {
+    e.preventDefault();
+    if (textDescription.value.length == '') {
+      alert('Recuerda, para conectar necesitas expresarte ');
+    } else {
+      if (editPostId === null){ // si no hay post a editar, agrega un nuevo post
+        await db.collection('post').add({
+          textDescription: textDescription.value
+        });
+      } else { // cuando se edita, se modifica el post selecionado
+        await db.collection('post').doc(editPostId).set({
+          textDescription: createPostTemplate(textDescription.value)
+        });
+      editPostId = null;
+    }
+  }
+  textDescription.value = '';
+  });
+};
+
+const realtimeListener = () => {
+  // real-time listener
+  db.collection('post').onSnapshot(snapshot => {
+    let changes = snapshot.docChanges();
+    changes.forEach(change => {
+      // console.log(change.doc.data());
+      if (change.type === 'added') {
+        viewPost(change.doc);
+      } else if (change.type === 'modified'){
+        let postsList = publicPost.querySelector('[data-id="' + change.doc.id + '"]');
+        postsList.querySelector('#postedTextId').textContent = change.doc.data().createPostTemplate(textDescription); // reescribir de forma inmediata el texte area. 
+      } else if (change.type === 'removed') {
+        let postsList = publicPost.querySelector('[data-id="' + change.doc.id + '"]');
+        publicPost.removeChild(postsList);
+      }
+    });
+  });
+};
+
+const viewPost = (doc) => {
+  const postsList = document.createElement('li');
+  const postedText = document.createElement('span');
+  const interactionElements = document.createElement('div');  
+  
+  postedText.id = ('postedTextId');
+  postsList.setAttribute('data-id', doc.id);
+  postedText.textContent = doc.data().textDescription;
+
+  postsList.className = ('li');
+  postedText.className = ('postedText');
+  interactionElements.className = 'interactionWrapper';
+
+  containerAddPost.appendChild(publicPost);
+  publicPost.appendChild(postsList);
+  postsList.appendChild(postedText);
+  postsList.appendChild(interactionElements);
+  interactionElements.appendChild(deleteUserPost());
+  interactionElements.appendChild(editUserPost());
+  interactionElements.appendChild(likeUserPost());
+  interactionElements.appendChild(commentUserPost());
+};
+
+const deleteUserPost = () => {
+  const deletePost = document.createElement('img');
+  deletePost.className = ('delete');
+  deletePost.src = './images/deletepost.svg';
+
+  // borrar posts
+  deletePost.addEventListener('click', (e) => {
+    e.stopPropagation();
+    const textId = e.target.parentElement.parentElement.getAttribute('data-id');
+    db.collection('post').doc(textId).delete();
+  });
+};
+
+const editUserPost = () => {
+  const edit = document.createElement('img');
+  edit.textContent = 'editar';
+  edit.className = ('edit');
+  edit.src = './images/editpost.svg';
+
+  // editar posts
+  edit.addEventListener('click', async (e) => {
+    e.stopPropagation();
+    editPostId = e.target.parentElement.parentElement.getAttribute('data-id'); // guardamos el id del post
+    const postData = await db.collection('post').doc(editPostId).get(); // pasamos la data del post a la variable postData
+    createPostTemplate(textDescription.value) = postData.data().createPostTemplate(textDescription);
+  });
+};
+
+const likeUserPost = () => {
+  const like = document.createElement('img');
+  like.className = ('likePost');
+  like.src = './images/likepost.svg';
+};
+
+const commentUserPost = () => {
+  const comment = document.createElement('img');
+  comment.className = ('commentPost');
+  comment.src = './images/commentpost.svg';
+};
