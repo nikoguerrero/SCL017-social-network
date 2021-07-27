25d8abbb3d3914779cae13e548a585f40904fa03
nikoguerrero <nclguerrero@gmail.com>
2021-07-27 15:21:26 -0400

    changing template strings to createElement in compose post modal

--- src/templates/components/composePostModal.js
+++ src/templates/components/composePostModal.js
@@ -1,30 +1,53 @@
-import { topNavBar } from './navbar.js';
 import { saveData } from './post.js';
 
 export const createPostModal = () => {
     const composePostContainer = document.createElement('div');
+    composePostContainer.id = 'composePostContainer';
     composePostContainer.className = 'composePostContainer';
-    const containerAddPost = document.createElement('section');
-    containerAddPost.className = 'containerAddPost';
-    const addPost = `
-    <div class="containerComposePost" id="containerPost">
-    <img src="./images/ejemploperfilfoto.png" class="feedPicProfile"> 
-    <textarea id="postBox" class="createPostText" placeholder="Descríbelo aquí"></textarea>
-    <button id="postButton" class="postButtonLink">compartir</button>
-    </div>`;
-  
-    containerAddPost.innerHTML = addPost;
-    
-    composePostContainer.appendChild(topNavBar());
+
+    const containerAddPost = document.createElement('section'); 
+    containerAddPost.className = 'containerAddPost'; // elemento de post.js, solo funciona en mobile
     composePostContainer.appendChild(containerAddPost);
-    const postBox = composePostContainer.querySelector('#postBox');
-    const sharePostButton = composePostContainer.querySelector('#postButton');
-    sharePostButton.addEventListener('click', () => {
+
+    const composePostModal = document.createElement('p');
+    composePostModal.id = 'containerPost';
+    composePostModal.className = 'containerComposePost';
+    composePostContainer.appendChild(composePostModal);
+
+    const modalHeader = document.createElement('div');
+    modalHeader.className = 'modalHeader';
+    composePostModal.appendChild(modalHeader);
+
+    const cancelPostLink = document.createElement('a');
+    cancelPostLink.id = 'cancelPostLink';
+    cancelPostLink.className = 'backLink';
+    cancelPostLink.innerHTML = 'Cancelar';
+    modalHeader.appendChild(cancelPostLink);
+
+    const userPicModal = document.createElement('img');
+    userPicModal.className = 'feedPicProfile';
+    userPicModal.src = './images/ejemploperfilfoto.png';
+    composePostModal.appendChild(userPicModal)
+
+    const postBox = document.createElement('textarea');
+    postBox.id = 'postBox';
+    postBox.className = 'createPostText';
+    postBox.placeholder= 'Descríbelo aquí';
+    composePostModal.appendChild(postBox);
+
+    const buttonPostModal = document.createElement('button');
+    buttonPostModal.id = 'postButton';
+    buttonPostModal.className = 'postButtonLink';
+    buttonPostModal.innerHTML = 'compartir';
+    composePostModal.appendChild(buttonPostModal);
+    
+    buttonPostModal.addEventListener('click', () => {
       saveData(postBox.value);
+      document.getElementById('root').removeChild(composePostContainer);
+    });
+
+    cancelPostLink.addEventListener('click', () => {
       document.getElementById('root').removeChild(composePostContainer);
     });
     return composePostContainer;
   };
-  
-
-  
