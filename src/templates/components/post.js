83ff6f8077ea4b2ca6c022390a2141b67c2fa353
nikoguerrero <nclguerrero@gmail.com>
2021-08-03 14:45:47 -0400

    added camera icon for uploading image button. Moved like functions to likePost.js

--- src/templates/components/post.js
+++ src/templates/components/post.js
@@ -1,6 +1,7 @@
 import { firebaseGetDatabase } from '../../lib/firebase.js';
 import { deleteButton } from './deletePost.js';
 import { editButton } from './editPost.js';
+import { likeButton } from './likePost.js';
 
 export const postTemplate = () => {
   const containerAddPost = document.createElement('section');
@@ -16,7 +17,11 @@ export const postTemplate = () => {
     <textarea id="text-description" class="createPostText" maxlength ="260" rows="2" colums="20" placeholder ="Descríbelo aquí"></textarea>
   </div>
   <div class="footerPost" id="footerPost">
-    <button id="uploadImage" class="uploadImgBtn">Sube una imagen</button>
+    <a id="uploadImage" class="uploadImgBtn">
+      <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M22.5352 16C21.8665 16 21.242 16.3342 20.8711 16.8906L19.4648 19H17C15.8954 19 15 19.8954 15 21V30C15 31.1046 15.8954 32 17 32H31C32.1046 32 33 31.1046 33 30V21C33 19.8954 32.1046 19 31 19H28.5352L27.1289 16.8906C26.758 16.3342 26.1335 16 25.4648 16H22.5352ZM22.5352 18H25.4648L26.8711 20.1094C27.242 20.6658 27.8665 21 28.5352 21H31V30H17V21H19.4648C20.1335 21 20.758 20.6658 21.1289 20.1094L22.5352 18ZM26 25C26 26.1046 25.1046 27 24 27C22.8954 27 22 26.1046 22 25C22 23.8954 22.8954 23 24 23C25.1046 23 26 23.8954 26 25ZM28 25C28 27.2091 26.2091 29 24 29C21.7909 29 20 27.2091 20 25C20 22.7909 21.7909 21 24 21C26.2091 21 28 22.7909 28 25Z" fill="#222222"/>
+      </svg>
+    </a>
     <button id="postButton" class="postButtonLink">Compartir</button>
   </div>
   </div>`;
@@ -137,64 +142,6 @@ export const saveData = async (textDescription) => {
     });
   
   }
-};
-
-export const likeButton = (likeCount) => {
-  const like = document.createElement('div');
-  like.className = 'likePost';
-  like.src = './images/likepost.svg';
-  like.id = 'like';
-
-  // si la cuenta de likes es mayor a 0, se imprime en pantalla
-  if (likeCount > 0) {
-    like.innerHTML = '<span class="likeCounter">' + likeCount + '</span>';
-  }
-
-  like.addEventListener('click',  (e) => {
-    e.stopPropagation();
-    const postId = e.target.parentElement.parentElement.getAttribute('data-id');
-    console.log(postId);
-    likePost(postId, e.target);
-  });
-  return like;
-};
-
-export const likePost = async (postId, likeBtn) => {
-  const postsRef = firebaseGetDatabase().collection('post');
-  const getPostData = await postsRef.doc(postId).get();
-  const postData = getPostData.data(); // obtenemos el objeto con la data del post
-  const currentUserId = firebase.auth().currentUser.uid; // obtenemos el id del usuario conectado
-
-  // si la propiedad "likes" no contiene la id del usuario, empuje la id al array
-  if (!postData.likes.includes(currentUserId)) {
-    // se añade animación de like
-    likeBtn.classList.add('is_liked');
-    likeBtn.classList.remove('is_not_liked');
-    postData.likes.push(currentUserId); // push agrega el id al array
-
-    // se agrega número de like si se hace like
-    likeBtn.innerHTML = postData.likes.length;
-    console.log('hiciste like');
-  } else {
-    // si la propiedad "likes" sí contiene la id del usuario, elimina el id del array
-    const idIndex = postData.likes.indexOf(currentUserId); // aquí buscamos el id del usuario en el array
-
-    // se quita animación de like (no funcionando bien)
-    likeBtn.classList.remove('is_liked');
-    likeBtn.classList.add('is_not_liked');
-    console.log('quitaste el like');
-    postData.likes.splice(idIndex, 1); // splice elimina el id del array
-
-    // se disminuye número de like si se hace dislike
-    if (postData.likes.length === 0) {
-      likeBtn.innerHTML = '';
-    } else {
-      likeBtn.innerHTML = postData.likes.length;
-    }
-  }
-  const result = await postsRef.doc(postId).update({ // actualizamos la propiedad like del post
-    likes: postData.likes
-  });
 };
 
 const commentButton = () => {
