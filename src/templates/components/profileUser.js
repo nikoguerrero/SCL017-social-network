ab729dbcf43c5f3ac4f30127c7160f2d11720d9c
nikoguerrero <nclguerrero@gmail.com>
2021-08-05 14:30:34 -0400

    update user profile photo

--- src/templates/components/profileUser.js
+++ src/templates/components/profileUser.js
@@ -1,3 +1,5 @@
+// import { firebaseGetDatabase } from '../lib/firebase.js';
+
 export const profileTemplate = (container) => {
   console.log('perfil');
   const containerFeed = container.querySelector('#containerFeedId');
@@ -111,6 +113,21 @@ export const editProfileModal = () => {
   userPicProfile.src = './images/ejemploperfilfoto.png';
   infoTextContainer.appendChild(userPicProfile);
 
+  const uploadPic = document.createElement('div');
+  uploadPic.id = 'uploadPicId';
+  uploadPic.className = 'uploadPic';
+
+  const uploadPicSvg = `
+    <a id="cameraIcon" class="uploadImgBtn">
+      <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M22.5352 16C21.8665 16 21.242 16.3342 20.8711 16.8906L19.4648 19H17C15.8954 19 15 19.8954 15 21V30C15 31.1046 15.8954 32 17 32H31C32.1046 32 33 31.1046 33 30V21C33 19.8954 32.1046 19 31 19H28.5352L27.1289 16.8906C26.758 16.3342 26.1335 16 25.4648 16H22.5352ZM22.5352 18H25.4648L26.8711 20.1094C27.242 20.6658 27.8665 21 28.5352 21H31V30H17V21H19.4648C20.1335 21 20.758 20.6658 21.1289 20.1094L22.5352 18ZM26 25C26 26.1046 25.1046 27 24 27C22.8954 27 22 26.1046 22 25C22 23.8954 22.8954 23 24 23C25.1046 23 26 23.8954 26 25ZM28 25C28 27.2091 26.2091 29 24 29C21.7909 29 20 27.2091 20 25C20 22.7909 21.7909 21 24 21C26.2091 21 28 22.7909 28 25Z" fill="#222222"/>
+      </svg>
+    </a>
+    <input style=display:none type="file" id="uploadImage"/>
+  `;
+  uploadPic.innerHTML = uploadPicSvg;
+  infoTextContainer.appendChild(uploadPic);
+
   const nameInput = document.createElement('input');
   nameInput.type ='text';
   nameInput.id = 'nameInput';
@@ -137,16 +154,89 @@ export const editProfileModal = () => {
   modalFooter.className = 'modalFooter';
   composePostModal.appendChild(modalFooter);
 
-
   const bottomPostButton = document.createElement('button');
   bottomPostButton.id = 'bottomButton';
   bottomPostButton.className = 'bottomPostButton';
   bottomPostButton.innerHTML = 'Compartir';
   modalFooter.appendChild(bottomPostButton);
 
-
   cancelPostLink.addEventListener('click', () => {
     document.getElementById('root').removeChild(composePostContainer);
   });
+
+  const cameraIconBtn = uploadPic.querySelector('#cameraIcon');
+  const uploadImage = uploadPic.querySelector('#uploadImage');
+  cameraIconBtn.addEventListener('click', () => {
+    uploadImage.click();
+  });
+
+  const uploadUserImg = (uploadImage) => {
+    const file = uploadImage.files[0];
+    const ref = firebase.storage().ref();
+    if (file) {
+      const nameFile = `${new Date()}-${file.name}`;
+      const metadata = {
+        contentType: file.type,
+      };
+      const task =  ref.child(nameFile).put(file, metadata);
+      showUploadedImg(task);
+    } else {
+      console.log('no existe ningun archivo');
+    }
+  }; 
+  
+  const showUploadedImg = (tasks) => {
+    tasks
+    .then((snapshot) => {
+      console.log(snapshot.ref.getDownloadURL());
+      return snapshot.ref.getDownloadURL();
+    })
+    .then((url) => {
+      const profilePhoto = url;
+
+      const user = firebase.auth().currentUser;
+      console.log(user);
+      user.updateProfile({
+        photoURL: profilePhoto,
+        displayName: nameInput.value
+      }).then(() => {
+        console.log('updatelogrado');
+        // Update successful
+        // ...
+      }).catch((error) => {
+        // An error occurred
+        // ...
+      });
+    })
+    .catch(console.error);
+  };
+  
+  bottomPostButton.addEventListener('click', async () => {
+    uploadUserImg(uploadImage);
+
+    // const userPicture = profilePhoto;
+    // console.log(userPicture);
+    // const username = nameInput.value;
+    // const userBio = bioInput.value;
+    // const userInterests = interestsInput.value;
+
+    // const user = firebase.auth().currentUser;
+    // console.log(user);
+    // user.updateProfile({
+    //   photoURL: userPicture,
+    //   displayName: username
+    // }).then(() => {
+    //   console.log('updatelogrado');
+    //   // Update successful
+    //   // ...
+    // }).catch((error) => {
+    //   // An error occurred
+    //   // ...
+    // });
+  });
+  
+
+  
   return composePostContainer;
 };
+
