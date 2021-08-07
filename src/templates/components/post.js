851105e1d723adedee8581ca617b6d59444bac4d
nikoguerrero <nclguerrero@gmail.com>
2021-08-07 12:50:24 -0400

    fixed order by on current user profile posts

--- src/templates/components/post.js
+++ src/templates/components/post.js
@@ -11,8 +11,8 @@ export const displayPosts = async (publicPost, userId) => {
       collection = await firebaseGetDatabase().collection('post').orderBy('timestamp', 'desc').get();
     } else {
       const collectionFilter = await firebaseGetDatabase().collection('post').where('userId', '==', userId);
-      // const orderCollection = await collectionFilter.orderBy('timestamp', 'desc');
-      collection = await collectionFilter.get();
+      const orderCollection = await collectionFilter.orderBy('timestamp', 'desc');
+      collection = await orderCollection.get();
     }
     collection.docs.forEach((doc) => {
       viewPost(doc, publicPost, false);
