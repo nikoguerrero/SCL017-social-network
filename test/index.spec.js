c48ba1e9f1933734bee7b0db7f37a03778ba9e60
GabbyHolics <karenthgabriela@gmail.com>
2021-08-09 12:40:43 -0400

    creating test

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,18 +1,18 @@
-// importamos la funcion que vamos a testear
-// import { initApp } from '../src/lib/index';
-import mockFirebase from '../__mocks__/firebase-mock.js';
-import { saveData } from '../src/templates/components/post.js';
+// // importamos la funcion que vamos a testear
+import { initApp } from '../src/lib/index';
+// // import mockFirebase from '../__mocks__/firebase-mock.js';
+// // import { saveData } from '../src/templates/components/post.js';
 
-global.firebase = mockFirebase();
-// describe('initApp', () => {
-//   it('debería ser una función', () => {
-//     expect(typeof initApp).toBe('function');
-//   });
-// });
-
-describe('saveData', () => {
-  it('collection post', async () => {
-    const data = await saveData('ratata');
-    expect(data).toBe('ratata');
+// global.firebase = mockFirebase();
+describe('initApp', () => {
+  it('debería ser una función', () => {
+    expect(typeof initApp).toBe('function');
   });
 });
+
+// describe('saveData', () => {
+//   it('collection post', async () => {
+//     const data = await saveData('ratata');
+//     expect(data).toBe('ratata');
+//   });
+// });
